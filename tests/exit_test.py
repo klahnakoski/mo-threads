89b7d4bb5dc354cde1e54ffc3dad1af10dd11003
Kyle Lahnakoski <klahnakoski@mozilla.com>
2019-09-09 17:15:19 -0400

    try this

--- tests/exit_test.py
+++ tests/exit_test.py
@@ -16,58 +16,58 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 
-print("strart imports")
+print("start imports")
 
-from mo_future import text_type
-from mo_logs import Log
-
-print("import threads")
-
-from mo_threads import Thread, Signal, MAIN_THREAD, Till, till
-
-
-def blame():
-    Log.warning("Got signal to stop timeout")
-
-
-print("make stop signal\n")
-
-
-please_stop = Signal()
-please_stop.then(blame)
-
-print("make please_stop\n")
-
-
-def timeout(please_stop):
-    print("set debug")
-    till.DEBUG=True
-    print("make timer")
-    timer = Till(seconds=20)
-    print("set blame")
-    timer.then(blame)
-    (timer | please_stop).wait()
-    if timer:
-        print("timer value: " + text_type(timer._go) + "\n")
-        print("problem with timer\n")
-    if please_stop:
-        print("problem with stop\n")
-    print("out of time\n")
-    please_stop.go()
-
-print("defined timeout\n")
-
-if please_stop:
-    print("stopped before thread\n")
-Thread.run("timeout", target=timeout, please_stop=please_stop)
-if please_stop:
-    print("stopped after thread\n")
-
-Log.note("you must type 'exit', and press Enter, or wait 20seconds")
-MAIN_THREAD.wait_for_shutdown_signal(allow_exit=True, please_stop=please_stop)
-
-if not please_stop:
-    Log.note("exit detected")
-else:
-    Log.note("timeout detected")
-please_stop.go()
+# from mo_future import text_type
+# from mo_logs import Log
+#
+# print("import threads")
+#
+# from mo_threads import Thread, Signal, MAIN_THREAD, Till, till
+#
+#
+# def blame():
+#     Log.warning("Got signal to stop timeout")
+#
+#
+# print("make stop signal\n")
+#
+#
+# please_stop = Signal()
+# please_stop.then(blame)
+#
+# print("make please_stop\n")
+#
+#
+# def timeout(please_stop):
+#     print("set debug")
+#     till.DEBUG=True
+#     print("make timer")
+#     timer = Till(seconds=20)
+#     print("set blame")
+#     timer.then(blame)
+#     (timer | please_stop).wait()
+#     if timer:
+#         print("timer value: " + text_type(timer._go) + "\n")
+#         print("problem with timer\n")
+#     if please_stop:
+#         print("problem with stop\n")
+#     print("out of time\n")
+#     please_stop.go()
+#
+# print("defined timeout\n")
+#
+# if please_stop:
+#     print("stopped before thread\n")
+# Thread.run("timeout", target=timeout, please_stop=please_stop)
+# if please_stop:
+#     print("stopped after thread\n")
+#
+# Log.note("you must type 'exit', and press Enter, or wait 20seconds")
+# MAIN_THREAD.wait_for_shutdown_signal(allow_exit=True, please_stop=please_stop)
+#
+# if not please_stop:
+#     Log.note("exit detected")
+# else:
+#     Log.note("timeout detected")
+# please_stop.go()
