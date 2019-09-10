1bb5f974d6640a065a06601856be7baa7bfb5467
Kyle Lahnakoski <klahnakoski@mozilla.com>
2019-09-09 20:29:39 -0400

    de-link stop signals

--- tests/exit_test.py
+++ tests/exit_test.py
@@ -16,31 +16,22 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 
-from time import sleep
-
-from mo_future import text_type
 from mo_logs import Log
 
-from mo_threads import Thread, Signal, MAIN_THREAD, Till, till
-
-
-please_stop = Signal()
+from mo_threads import Thread, Signal, MAIN_THREAD, Till
 
 def timeout(please_stop):
     timer = Till(seconds=20)
     (timer | please_stop).wait()
-    please_stop.go()
+    if not please_stop:
+        Log.note("exit detected")
+    else:
+        Log.note("timeout detected")
 
-Thread.run("timeout", target=timeout, please_stop=please_stop)
+Thread.run("timeout", target=timeout)
 
 Log.note("you must type 'exit', and press Enter, or wait 20seconds")
 try:
-    MAIN_THREAD.wait_for_shutdown_signal(allow_exit=True, please_stop=please_stop, wait_forever=False)
+    MAIN_THREAD.wait_for_shutdown_signal(allow_exit=True, wait_forever=False)
 except Exception as e:
     Log.error("can not wait", cause=e)
-
-if not please_stop:
-    Log.note("exit detected")
-else:
-    Log.note("timeout detected")
-please_stop.go()
