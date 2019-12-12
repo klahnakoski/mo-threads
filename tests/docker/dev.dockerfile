33b68a1b0cfe713e0f12db65f0c2d92d2d80a53b
Kyle Lahnakoski <kyle@lahnakoski.com>
2019-12-12 12:38:32 -0500

    updates from other projects

--- tests/docker/dev.dockerfile
+++ tests/docker/dev.dockerfile
@@ -1,4 +1,4 @@
-FROM python:2.7.15
+FROM python:3.7.2
 
 WORKDIR /app
 RUN mkdir tests
---
+++ tests/exit_test.py
@@ -0,0 +1,37 @@
+# encoding: utf-8
+#
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+
+###############################################################################
+# Intended to test exit behaviour from timeout, SIGINT (CTRL-C), or "exit"
+###############################################################################
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+from mo_logs import Log
+
+from mo_threads import Thread, Signal, MAIN_THREAD, Till
+
+def timeout(please_stop):
+    Log.note("timout waiting")
+    (Till(seconds=20) | please_stop).wait()
+    if please_stop:
+        Log.note("EXIT DETECTED")
+    else:
+        Log.note("timeout detected")
+
+Thread.run("timeout", target=timeout)
+
+Log.note("you must type 'exit', and press Enter, or wait 20seconds")
+try:
+    MAIN_THREAD.wait_for_shutdown_signal(allow_exit=True, wait_forever=False)
+except Exception as e:
+    Log.error("can not wait", cause=e)
