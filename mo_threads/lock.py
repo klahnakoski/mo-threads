cb8eb5fb4ab2d0d54a14b18ffb053aee1b9a9455
Kyle Lahnakoski <kyle@lahnakoski.com>
2023-08-23 19:52:21 -0400

    updates from other projects

--- mo_threads/lock.py
+++ mo_threads/lock.py
@@ -13,6 +13,7 @@
 
 
 from mo_future import allocate_lock as _allocate_lock, decorate
+from mo_json import quote
 from mo_logs import logger
 
 from mo_threads.signals import Signal
@@ -35,19 +36,19 @@ class Lock(object):
         self.waiting = None
 
     def __enter__(self):
-        self.debug and print(f"acquire  lock {self.name}")
+        self.debug and print(f"acquire  lock {quote(self.name)}")
         self.lock.acquire()
-        self.debug and print(f"acquired lock {self.name}")
+        self.debug and print(f"acquired lock {quote(self.name)}")
         return self
 
     def __exit__(self, a, b, c):
         if self.waiting:
-            self.debug and print(f"signaling {len(self.waiting)} waiters on {self.name}")
+            self.debug and print(f"signaling {len(self.waiting)} waiters on {quote(self.name)}")
             # TELL ANOTHER THAT THE LOCK IS READY SOON
             waiter = self.waiting.pop()
             waiter.go()
         self.lock.release()
-        self.debug and print(f"released lock {self.name}")
+        self.debug and print(f"released lock {quote(self.name)}")
 
     def wait(self, till=None):
         """
@@ -55,33 +56,35 @@ class Lock(object):
         :param till: WHEN TO GIVE UP WAITING FOR ANOTHER THREAD TO SIGNAL, LOCK IS STILL ACQUIRED
         :return: True IF SIGNALED TO GO, False IF till WAS SIGNALED
         """
-        self.debug and print(f"as waiter on {self.name}")
+        self.debug and print(f"wait on lock: {quote(self.name)}")
         waiter = Signal()
         if self.waiting:
             # TELL ANOTHER THAT THE LOCK IS READY SOON
-            self.debug and print(f"inform other on {self.name}")
+            self.debug and print(f"inform other on {quote(self.name)}")
             other = self.waiting.pop()
             other.go()
-            self.debug and print(f"waiting with {len(self.waiting)} others on {self.name}")
+            self.debug and print(f"waiting with {len(self.waiting)} others on {quote(self.name)}")
             self.waiting.insert(0, waiter)
         else:
-            self.debug and print(f"waiting by self on {self.name}")
+            self.debug and print(f"waiting by self on {quote(self.name)}")
             self.waiting = [waiter]
 
+        both = waiter | till
         try:
             self.lock.release()
-            self.debug and print(f"out of lock {self.name}")
-            (waiter | till).wait()
+            self.debug and print(f"out of lock {quote(self.name)}")
+            self.debug and print(f"wait on  {both.name}")
+            both.wait()
             self.debug and print(f"done minimum wait (for signal {till.name})")
         except Exception as cause:
             logger.warning("problem", cause=cause)
         finally:
             self.lock.acquire()
-            self.debug and print(f"re-acquired lock {self.name}")
+            self.debug and print(f"re-acquired lock: {quote(self.name)}")
 
         try:
             self.waiting.remove(waiter)
-            self.debug and print(f"removed own signal from {self.name}")
+            self.debug and print(f"removed own signal from {quote(self.name)}")
         except Exception:
             pass
 
