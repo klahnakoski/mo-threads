357b6403536c658f584350fc2b1e7e59c52b7d34
Kyle Lahnakoski <kyle@lahnakoski.com>
2023-10-14 06:40:39 -0400

    update version number

--- packaging/setup.py
+++ packaging/setup.py
@@ -15,6 +15,6 @@ setup(
     name='mo-threads',
     packages=["mo_threads"],
     url='https://github.com/klahnakoski/mo-threads',
-    version='6.447.23285',
+    version='6.447.23287',
     zip_safe=False
 )
