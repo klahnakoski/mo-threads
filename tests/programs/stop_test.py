28ee6c404c5e6b7168091bc8fa988c032afd2b4a
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-01-24 00:38:25 -0500

    revert

--- tests/programs/stop_test.py
+++ tests/programs/stop_test.py
@@ -13,7 +13,7 @@
 
 from mo_logs import logger
 
-from mo_threads import Signal, Thread, wait_for_shutdown_signal
+from mo_threads import Signal, Thread, wait_for_shutdown_signal, stop_main_thread
 from mo_threads.till import Till
 
 please_stop = Signal()
@@ -21,6 +21,7 @@ please_stop = Signal()
 
 def timeout(please_stop):
     (Till(seconds=1) | please_stop).wait()
+    stop_main_thread()
 
 
 Thread.run("timeout", target=timeout, please_stop=please_stop)
