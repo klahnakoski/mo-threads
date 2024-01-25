7e0c3acd2bd159597097fa4e72538a0e0457bbc1
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-01-24 21:02:56 -0500

    fix test timeout

--- tests/programs/stop_test.py
+++ tests/programs/stop_test.py
@@ -13,14 +13,18 @@
 
 from mo_logs import logger
 
-from mo_threads import Signal, Thread, wait_for_shutdown_signal
+from mo_threads import Signal, Thread, stop_main_thread, wait_for_shutdown_signal
 from mo_threads.till import Till
 
 please_stop = Signal()
 
 
 def timeout(please_stop):
-    (Till(seconds=1) | please_stop).wait()
+    logger.info("starting")
+    (Till(seconds=5) | please_stop).wait()
+    logger.info("time up")
+    stop_main_thread()
+    logger.info("done")
 
 
 Thread.run("timeout", target=timeout, please_stop=please_stop)
